480daa1d460a7d6b0efc8f458d268c774e093f19
maclomaclee <50185441+maclomaclee@users.noreply.github.com>
2024-01-31 21:47:31 +0000

    31012408

--- wrangling/data_wrangle_script.R
+++ wrangling/data_wrangle_script.R
@@ -78,7 +78,7 @@ recent_reconciledID <- LSR1_reconciled %>%
 
 # Filter all records that have a StudyIdStr and AnnotatorIdStr that is in the reconciled_study_annotator_pairs to get final set of reconciled data
 reconciled_records_unique <- LSR1_reconciled %>%
-  semi_join(recent_reconciledID, by = "StudyId")
+  semi_join(recent_reconciledID, by = "StudyId", "InvestigatorId")
 
 # Rearrange rows to be grouped by studies and then experiment within studies. Reorder columns for readability
 reconciled_records <- reconciled_records_unique %>%
